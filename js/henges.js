9c832bfd94d35cf363fefcd484a5337b4250e1dd
Alex Robertson <alexrson@gmail.com>
2014-12-26 12:01:15 -0800

    GIS initial commit

---
+++ js/henges.js
@@ -0,0 +1,15 @@
+/**
+ * houses.js
+ *
+ * Henges' approximate coordinates.
+ */
+
+var HENGE_STARTS = 
+{
+    "Main Henge": {lat: 42.362553, lng: -71.088527}
+};
+
+var HENGE_ENDS = 
+{
+    "Main Henge": {lat: 42.363409, lng: -71.099750}
+};
